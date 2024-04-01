4ba25b5dab1388994c05d3d2666decdc0ff35e66
denoseu <denisefeliciatiowanni@gmail.com>
2024-04-01 14:52:09 +0700

    Update keyboard.c

--- src/keyboard.c
+++ src/keyboard.c
@@ -3,8 +3,16 @@
 #include "header/text/framebuffer.h"
 #include "header/cpu/portio.h"
 #include "header/stdlib/string.h"
+#include "stdbool.h"
 
-struct KeyboardDriverState KeyboardDriverState;
+struct KeyboardDriverState keyboard_gobyos = {
+    .read_extended_mode = false,
+    .keyboard_input_on = false,
+    .keyboard_buffer = '\0',
+};
+
+static uint8_t cursor_row = 0;
+static uint8_t cursor_col = 0;
 
 
 const char keyboard_scancode_1_to_ascii_map[256] = {
@@ -27,25 +35,28 @@ const char keyboard_scancode_1_to_ascii_map[256] = {
       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
 };
 
+// nanti bikin untuk shift dan capslock
+
 /* -- Driver Interfaces -- */
 
 // Activate keyboard ISR / start listen keyboard & save to buffer
 void keyboard_state_activate(void){
-    KeyboardDriverState.keyboard_input_on = true;
+    activate_keyboard_interrupt();
+    keyboard_gobyos.keyboard_input_on = true;
 
 }
 
 // Deactivate keyboard ISR / stop listening keyboard interrupt
 void keyboard_state_deactivate(void){
-    KeyboardDriverState.keyboard_input_on = false;
+    keyboard_gobyos.keyboard_input_on = false;
 }
 
 // Get keyboard buffer value and flush the buffer - @param buf Pointer to char buffer
 void get_keyboard_buffer(char *buf){
-    *buf = KeyboardDriverState.keyboard_buffer;
+    *buf = keyboard_gobyos.keyboard_buffer;
 
     // Flush the keyboard buffer
-    KeyboardDriverState.keyboard_buffer = '\0';
+    keyboard_gobyos.keyboard_buffer = '\0';
 }
 
 /* -- Keyboard Interrupt Service Routine -- */
@@ -54,18 +65,54 @@ void get_keyboard_buffer(char *buf){
  * Handling keyboard interrupt & process scancodes into ASCII character.
  * Will start listen and process keyboard scancode if keyboard_input_on.
  */
-void keyboard_isr(void){
-    // mendapat scancode scancode dari port KEYBOARD_DATA_PORT
-    uint8_t scancode = in(KEYBOARD_DATA_PORT);
 
-    // Jika keyboard_input_on bernilai true
-    if (KeyboardDriverState.keyboard_input_on) {
-        // mengubah scancode menjadi ASCII character
+void keyboard_isr(void){
+
+    if (keyboard_gobyos.keyboard_input_on) {
+        uint8_t scancode = in(KEYBOARD_DATA_PORT);
         char ascii_char = keyboard_scancode_1_to_ascii_map[scancode];
-        // menyimpan karakter ASCII ke keyboard_buffer
-        KeyboardDriverState.keyboard_buffer = ascii_char;
+        
+        // Handle special ASCII characters
+        if (ascii_char == '\n') {
+            // Move the cursor to the beginning of the next line
+            cursor_row++;
+            cursor_col = 0;  // Reset column to 0 for the next line
+            framebuffer_set_cursor(cursor_row, cursor_col);
+        } else if (ascii_char == '\b') {
+            // Move the cursor back one position
+            if (cursor_col > 0) {
+                cursor_col--;
+                framebuffer_write(cursor_row, cursor_col, ' ', 0x07, 0x00);
+                framebuffer_set_cursor(cursor_row, cursor_col);
+            } else if (cursor_row > 0) {
+                cursor_row--;
+                cursor_col = 79;  // Move to the last column of the previous line
+                framebuffer_write(cursor_row, cursor_col, ' ', 0x07, 0x00);
+                framebuffer_set_cursor(cursor_row, cursor_col);
+            }
+        } else if (ascii_char == '\t') {
+            // Move the cursor to the next tab stop
+            for (int i = 0; i < 5; i++) {
+              if (cursor_col > 79) {
+                cursor_col = 0;
+                cursor_row++;
+              }
+              framebuffer_write(cursor_row, cursor_col, ' ', 0x07, 0x00);
+              cursor_col++;
+              framebuffer_set_cursor(cursor_row, cursor_col);
+            }
+        } else {
+            // Check if cursor reaches end of line
+            if (cursor_col > 79) {
+                cursor_row++;
+                cursor_col = 0;  // Reset column to 0 for the next line
+            }
+            // Regular character, update cursor position
+            framebuffer_write(cursor_row, cursor_col, ascii_char, 0x07, 0x00);
+            cursor_col++;
+            framebuffer_set_cursor(cursor_row, cursor_col);
+        }
     }
 
-    // pic_ack() ke IRQ1
     pic_ack(IRQ_KEYBOARD);
 }
