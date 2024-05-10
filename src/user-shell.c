9938b56bfc408122d742be6884c26e90aac345b7
debrinashika <debrina.veisha@gmail.com>
2024-05-10 12:21:01 +0700

    fix json

--- src/user-shell.c
+++ src/user-shell.c
@@ -20,40 +20,49 @@ void print(char *buf, uint8_t color){
 
 
 int main(void) {
-    char buf[255];
-    buf[254] = '\0';
+    // char buf[255];
+    // buf[254] = '\0';
+    char args_val[2048];
+    // int args_info[128][2];
+    // char path_str[2048];
+
+   print("GobyOS-IF2230", BIOS_LIGHT_GREEN);
+    interrupt (4, (uint32_t) args_val, 2048, 0x0);
+    interrupt(7, 0, 0, 0);
     while (true) {
         print("GobyOS-IF2230", BIOS_LIGHT_GREEN);
         print(":", BIOS_GREY);
-        char *arg = buf;
-        uint8_t len;
-        if (strcmp(arg, "cd")==1){
+        interrupt (4, (uint32_t) args_val, 2048, 0x0);
+        interrupt(7, 0, 0, 0);
+        // char *arg = buf;`
+        // uint8_t len;
+        // if (strcmp(arg, "cd")==1){
 
-        } 
-        else if (!strcmp(arg, "mkdir")==1){
+        // } 
+        // else if (!strcmp(arg, "mkdir")==1){
 
-        }
-        else if (strcmp(arg, "ls")==1){
+        // }
+        // else if (strcmp(arg, "ls")==1){
 
-        }
-        else if (strcmp(arg, "cp")==1){
+        // }
+        // else if (strcmp(arg, "cp")==1){
 
-        }
-        else if (strcmp(arg, "cat")==1){
+        // }
+        // else if (strcmp(arg, "cat")==1){
 
-        }
-        else if (strcmp(arg, "mv")==1){
+        // }
+        // else if (strcmp(arg, "mv")==1){
 
-        }
-        else if (strcmp(arg, "rm")==1){
+        // }
+        // else if (strcmp(arg, "rm")==1){
 
-        }
-        else if (strcmp(arg, "cp")==1){
+        // }
+        // else if (strcmp(arg, "cp")==1){
 
-        }
-        else if (strcmp(arg, "find")==1){
+        // }
+        // else if (strcmp(arg, "find")==1){
 
-        }
+        // }
     return 0;
     }
 }
